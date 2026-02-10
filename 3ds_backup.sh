a6c673d529931b4edb6c1c62ed9e136bf7e945e4
carnivuth <matti200042@gmail.com>
2026-02-10 14:42:01 +0100

    added first version of multiple backups

--- 3ds_backup.sh
+++ 3ds_backup.sh
@@ -4,9 +4,11 @@ log_inner() {
         echo "[${BASH_SOURCE##*/}:${1^^}] ${FUNCNAME[2]}@${BASH_LINENO[1]}: ${*:2}"
 }
 
-# check on env vars
-if [[ -z $FTPD_3DS_ADDRESS ]]; then log_inner error "Set server address trough FTPD_3DS_ADDRESS env var"; exit 1; fi
-if [[ -z $FTPD_3DS_PORT ]]; then FTPD_3DS_PORT=21; exit 1; fi
+# check if 3ds address variable is set, exit otherwise
+if [[ -z $FTPD_3DS_ADDRESSES ]]; then log_inner error "Set server address trough FTPD_3DS_ADDRESSES env var"; exit 1; fi
+
+# setting default port
+if [[ -z $FTPD_3DS_PORT ]]; then FTPD_3DS_PORT=21; fi
 
 # directories
 if [[ -z $BASE_DIR ]];then BASE_DIR=/var/lib/3ds_backup; fi
@@ -16,47 +18,75 @@ if [[ -z $BACKUP_DEST ]];then BACKUP_DEST="$BASE_DIR/backups"; fi
 if [[ ! -d "$BACKUP_DEST" ]]; then mkdir -p "$BACKUP_DEST"; fi
 
 # stat file to share information between cron instances of the script
-if [[ -z $STAT_FILE ]];then STAT_FILE="/tmp/status"; fi
-if [[ ! -f "$STAT_FILE" ]]; then echo 1 > "$STAT_FILE"; fi
+if [[ -z $STAT_DIR ]];then STAT_DIR="/tmp/backup_3ds/status"; fi
+if [[ ! -d "$STAT_DIR" ]]; then mkdir -p "$STAT_DIR"; fi
 
 # 3ds directory to backup
 if [[ -z $BACKUP_SRC ]];then BACKUP_SRC="/"; fi
 
 function backup(){
 
-  # check if 3ds ftp server is up
-  if ! nc -z -w1 "$FTPD_3DS_ADDRESS" "$FTPD_3DS_PORT"; then log_inner info "3ds is not listening for ftp connections"; exit 0; fi
+  IFS=';' read -ra addresses <<< "$FTPD_3DS_ADDRESSES"
+  for address in ${addresses[@]}; do
 
-  # check if 3ds backup has been made
-  if [[ $(cat "$STAT_FILE") == "0" ]]; then log_inner info "backup has already been made at $(stat -c '%y' "$STAT_FILE")"; exit 0; fi
+    # set status file variable
+    STAT_FILE="${STAT_DIR}/${address}"
+    if [[ ! -f "$STAT_FILE" ]]; then echo 1 > "$STAT_FILE"; fi
 
-  # check if 3ds backup is running
-  if [[ $(cat "$STAT_FILE") == "2" ]]; then log_inner info "backup is running since $(stat -c '%y' "$STAT_FILE")"; exit 0; fi
+    # setting backup dir for the specific 3ds
+    if [[ -z $HOST_DIR ]];then HOST_DIR="${BACKUP_DEST}/${address}"; fi
+    if [[ ! -d "$HOST_DIR" ]]; then mkdir -p "$HOST_DIR"; fi
 
-  log_inner info "starting backup of 3DS address at $FTPD_3DS_ADDRESS:$FTPD_3DS_PORT"
-  # setting status on status file to avoid running multiple backup jobs
-  echo 2 > "$STAT_FILE"
+    # check if 3ds backup is not running
+    if [[ $(cat "$STAT_FILE") != "2" ]]; then
 
-  timestamp="$(date +%s)"
-  IFS=';' read -ra dirs <<< "$BACKUP_SRC"
-  for dir in ${dirs[@]}; do
+      # check if 3ds backup has not been made
+      if [[ $(cat "$STAT_FILE") != "0" ]]; then
 
-    dirname="$(echo "$dir" | sed 's/\//-/g')"
-    mkdir "$BACKUP_DEST/${timestamp}_$dirname"
-    log_inner info "creating backup $BACKUP_DEST/${timestamp}_$dir of $dirname"
+        # check if 3ds ftp server is up
+        if nc -z -w1 "$address" "$FTPD_3DS_PORT"; then
+          # setting lock file to avoid running multiple backup jobs in parallel on the same 3DS
+          echo 2 > "$STAT_FILE"
+          log_inner info "starting backup of 3DS at $address:$FTPD_3DS_PORT"
 
-    # check for error codes and print error otherwise
-    if ncftpget -R -v -u "$FTPD_3DS_USERNAME" -p "$FTPD_3DS_PASSWORD" -P "$FTPD_3DS_PORT" "$FTPD_3DS_ADDRESS" "$BACKUP_DEST/${timestamp}_${dirname}" "$dir"; then
-      tar -czf "$BACKUP_DEST/${timestamp}_${dirname}.tar.gz" -C "$BACKUP_DEST" "${timestamp}_${dirname}" || log_inner error "error archiving ${BACKUP_DEST}/${timestamp}_${dirname}"
-      rm -r "$BACKUP_DEST/${timestamp}_$dirname"
-      log_inner info "done backup $BACKUP_DEST/${timestamp}_$dirname"
+          timestamp="$(date +%s)"
+
+          IFS=';' read -ra dirs <<< "$BACKUP_SRC"
+          for dir in ${dirs[@]}; do
+
+            dirname="$(echo "$dir" | sed 's/\//-/g')"
+            mkdir "$HOST_DIR/${timestamp}_$dirname"
+            log_inner info "creating backup $HOST_DIR/${timestamp}_$dir of $dirname"
+
+            # check for error codes and print error otherwise
+            if ncftpget -R -v -u "$FTPD_3DS_USERNAME" -p "$FTPD_3DS_PASSWORD" -P "$FTPD_3DS_PORT" "$address" "$HOST_DIR/${timestamp}_${dirname}" "$dir"; then
+              tar -czf "$HOST_DIR/${timestamp}_${dirname}.tar.gz" -C "$HOST_DIR" "${timestamp}_${dirname}" || log_inner error "error archiving ${HOST_DIR}/${timestamp}_${dirname}"
+              rm -r "${HOST_DIR}/${timestamp}_$dirname"
+              log_inner info "done backup ${HOST_DIR}/${timestamp}_$dirname"
+            else
+              log_inner error "error in downloading ${dir} from ${address}"
+            fi
+          done
+
+          # setting status file to "backup done" to avoid consequent backups
+          echo 0 > "$STAT_FILE"
+
+        else
+          log_inner info "3ds is not listening for ftp connections"
+        fi
+
+      else
+        log_inner info "backup has already been made at $(stat -c '%y' "$STAT_FILE")";
+      fi
+
     else
-      log_inner error "error in downloading $dir from $FTPD_3DS_ADDRESS"
+      log_inner info "backup is running since $(stat -c '%y' "$STAT_FILE")";
     fi
+
   done
 
-  # setting status file to "backup done" to avoid consequent backups
-  echo 0 > "$STAT_FILE"
+
+
 }
 
 function reset(){
--- Dockerfile
+++ Dockerfile
@@ -12,9 +12,9 @@ RUN chown root:root /etc/crontab
 
 # add project files
 WORKDIR /usr/local/bin
-COPY 3ds_backup.sh .
+COPY backup_3ds.sh .
 COPY start.sh .
 
-RUN chmod +x 3ds_backup.sh start.sh
+RUN chmod +x backup_3ds.sh start.sh
 
 CMD [ "./start.sh" ]
