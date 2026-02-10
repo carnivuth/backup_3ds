4a0a89673b48ebc887bd86dc6d44675495c58313
carnivuth <matti200042@gmail.com>
2026-02-10 15:08:55 +0100

    increased mok data size

--- create_mok_data.sh
+++ create_mok_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p debug/data{1,2}
 for file in {test1,test2,test3}; do
-head -c 100 /dev/urandom > debug/data1/$file
-head -c 100 /dev/urandom > debug/data2/$file
+head -c 1000000 /dev/urandom > debug/data1/$file
+head -c 1000000 /dev/urandom > debug/data2/$file
 done
 
